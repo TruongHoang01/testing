b328eba0e1f3c2b7850de5bfaf57abbfd8875c61
TruongHoang01 <banhathoang01@gmail.com>
2024-06-28 21:12:14 +0700

    fix

--- js.js
+++ js.js
@@ -2,7 +2,7 @@ let age = 18;
 console.log(age);
 
 const btn = document.querySelector(".btn-click");
-// btn.addEventListener("click",()=>{
-//     if(btn.textContent == "Chưa click") btn.textContent = "Đã click";
-//     else if(btn.textContent == "Đã click") btn.textContent = "Chưa click"
-// })
+btn.addEventListener("click",()=>{
+    if(btn.textContent == "Chưa click") btn.textContent = "Đã click";
+    else if(btn.textContent == "Đã click") btn.textContent = "Chưa click"
+})
