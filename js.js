44283de432f8ff2c3fab1da993850512680a9b61
TruongHoang01 <banhathoang01@gmail.com>
2024-06-28 21:06:44 +0700

    abc

---
+++ js.js
@@ -0,0 +1,8 @@
+let age = 18;
+console.log(age);
+
+const btn = document.querySelector(".btn-click");
+// btn.addEventListener("click",()=>{
+//     if(btn.textContent == "Chưa click") btn.textContent = "Đã click";
+//     else if(btn.textContent == "Đã click") btn.textContent = "Chưa click"
+// })
